d4b43562c05efb9baf0264253e843a7868ec4456
krafts <varun.net@gmail.com>
2017-01-20 18:28:54 -0500

    tabs to spaces

--- bash_history_search.sh
+++ bash_history_search.sh
@@ -2,8 +2,8 @@
 
 if command -v rg > /dev/null 2>&1
 then
-	rg "$@" ~/.bash_history*
+  rg "$@" ~/.bash_history*
 elif command -v grep > /dev/null 2>&1
 then
-	grep -R "$@" ~/.bash_history*
+  grep -R "$@" ~/.bash_history*
 fi
