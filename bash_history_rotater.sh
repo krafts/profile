379cf7b3ff393dc5f4f82d8d70a0e57688003d93
krafts <varun.net@gmail.com>
2017-02-09 11:34:36 -0500

    bash history rotater: using separate echo commands to add a newline
    for some reason echo -e is printing -e in the output as well

--- bash_history_rotater.sh
+++ bash_history_rotater.sh
@@ -25,7 +25,8 @@ if [ -s "$BASH_HIST" -a "$BASH_HIST" -nt "$BACKUP" ]; then
     # create new backup, leave last few commands and reinitialize
     mv -f $BASH_HIST $BACKUP
     tail -n$KEEP $BACKUP > $BASH_HIST
-    echo -e "#$(date +%s)\nls" > $BASH_HIST
+    echo "#$(date +%s)" > $BASH_HIST
+    echo "ls" >> $BASH_HIST
     history -r
   fi
 fi
