4e12e3569bf4d3d40c6cc4af68123432ebcb0656
krafts <varun.net@gmail.com>
2018-07-31 11:49:59 -0400

    virtualenv: using printf instead of echo to avoid issues in the terminal

--- get_virtualenv_name.sh
+++ get_virtualenv_name.sh
@@ -6,4 +6,4 @@ fi
 
 virtualenv_name=$(basename $VIRTUAL_ENV)
 
-echo -e "[\033[36m\033[1m$virtualenv_name\033[21m\033[39m\033[0m]"
+printf "[\033[36m\033[1m$virtualenv_name\033[21m\033[39m\033[0m\033[0m]"
