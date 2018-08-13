2343757bae6aea6c85625740c080a8e977fff7c4
krafts <varun.net@gmail.com>
2018-08-13 16:10:07 -0400

    virtualenv: removing all coloring and text formatting options

--- get_virtualenv_name.sh
+++ get_virtualenv_name.sh
@@ -6,4 +6,8 @@ fi
 
 virtualenv_name=$(basename $VIRTUAL_ENV)
 
-printf "[\033[36m\033[1m$virtualenv_name\033[21m\033[39m\033[0m\033[0m]"
+#printf "[\033[36m\033[1m$virtualenv_name\033[21m\033[39m\033[0m\033[0m]"
+#printf "[\033[36m\033[1m${virtualenv_name}\033[21m\033[39m]"
+#printf "(\e[1m\e[36m${virtualenv_name}\e[39m\e[21m\e[0m)"
+#printf "[$virtualenv_name]"
+printf "[$virtualenv_name]"
