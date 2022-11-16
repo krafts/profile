399e21e08244ca1319078805adbf16ed5a3305e5
krafts <varun.net@gmail.com>
2022-11-16 00:59:59 -0500

    wezterm: set tab bar, window padding and visual bell.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,6 +6,47 @@ local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
 -- The filled in variant of the > symbol
 local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
 
+
+wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config, hover, max_width)
+    local edge_background = '#0b0022'
+    local background = '#1b1032'
+    local foreground = '#808080'
+
+    if tab.is_active then
+      background = '#edad13'
+      foreground = '#1353ed'
+    elseif not tab.is_active then
+      background = '#1353ed'
+      foreground = '#edad13'
+    -- elseif hover then
+    --   background = '#3b3052'
+    --   foreground = '#909090'
+    end
+
+    local edge_foreground = background
+
+    -- ensure that the titles fit in the available space,
+    -- and that we have room for the edges.
+    local title = wezterm.truncate_right(tab.active_pane.title, max_width - 2)
+    --local tab_id = tab.tab_id
+    local tab_index = tab.tab_index
+
+    return {
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = SOLID_LEFT_ARROW },
+      { Background = { Color = background } },
+      { Foreground = { Color = foreground } },
+      { Text = "   ".. tab_index + 1 .. ": " .. title .. "   "},
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = SOLID_RIGHT_ARROW },
+    }
+  end
+)
+
 return {
   font = wezterm.font ('FiraCode Nerd Font Mono', {weight='Bold', stretch='Normal', style=Normal}),
   font_size = 15.0,
@@ -108,29 +149,97 @@ return {
     quick_select_label_fg = { Color = '#ffffff' },
     quick_select_match_bg = { AnsiColor = 'Navy' },
     quick_select_match_fg = { Color = '#ffffff' },
+
+    tab_bar = {
+      -- The color of the strip that goes along the top of the window
+      -- (does not apply when fancy tab bar is in use)
+      background = '#0b0022',
+
+  --     -- The active tab is the one that has focus in the window
+  --     active_tab = {
+  --       -- The color of the background area for the tab
+  --       bg_color = '#edad13',
+  --       -- The color of the text for the tab
+  --       fg_color = '#1353ed',
+
+  --       -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+  --       -- label shown for this tab.
+  --       -- The default is "Normal"
+  --       intensity = 'Bold',
+
+  --       -- Specify whether you want "None", "Single" or "Double" underline for
+  --       -- label shown for this tab.
+  --       -- The default is "None"
+  --       underline = 'None',
+
+  --       -- Specify whether you want the text to be italic (true) or not (false)
+  --       -- for this tab.  The default is false.
+  --       italic = false,
+
+  --       -- Specify whether you want the text to be rendered with strikethrough (true)
+  --       -- or not for this tab.  The default is false.
+  --       strikethrough = false,
+  --     },
+
+  --     -- Inactive tabs are the tabs that do not have focus
+  --     inactive_tab = {
+  --       bg_color = '#1353ed',
+  --       fg_color = '#edad13',
+
+  --       -- The same options that were listed under the `active_tab` section above
+  --       -- can also be used for `inactive_tab`.
+  --     },
+
+  --     -- You can configure some alternate styling when the mouse pointer
+  --     -- moves over inactive tabs
+  --     inactive_tab_hover = {
+  --       bg_color = '#3b3052',
+  --       fg_color = '#909090',
+  --       italic = true,
+
+  --       -- The same options that were listed under the `active_tab` section above
+  --       -- can also be used for `inactive_tab_hover`.
+  --     },
+
+  --     -- The new tab button that let you create new tabs
+  --     new_tab = {
+  --       bg_color = '#1b1032',
+  --       fg_color = '#808080',
+
+  --       -- The same options that were listed under the `active_tab` section above
+  --       -- can also be used for `new_tab`.
+  --     },
+
+  --     -- You can configure some alternate styling when the mouse pointer
+  --     -- moves over the new tab button
+  --     new_tab_hover = {
+  --       bg_color = '#3b3052',
+  --       fg_color = '#909090',
+  --       italic = true,
+
+  --       -- The same options that were listed under the `active_tab` section above
+  --       -- can also be used for `new_tab_hover`.
+  --     },
+    },
+
+    visual_bell = '#ffffff',
   },
 
   tab_bar_at_bottom = true,
-  -- tab_bar_style = {
-  --   active_tab_left = wezterm.format {
-  --     { Background = { Color = '#0b0022' } },
-  --     { Foreground = { Color = '#2b2042' } },
-  --     { Text = SOLID_LEFT_ARROW },
-  --   },
-  --   active_tab_right = wezterm.format {
-  --     { Background = { Color = '#0b0022' } },
-  --     { Foreground = { Color = '#2b2042' } },
-  --     { Text = SOLID_RIGHT_ARROW },
-  --   },
-  --   inactive_tab_left = wezterm.format {
-  --     { Background = { Color = '#0b0022' } },
-  --     { Foreground = { Color = '#1b1032' } },
-  --     { Text = SOLID_LEFT_ARROW },
-  --   },
-  --   inactive_tab_right = wezterm.format {
-  --     { Background = { Color = '#0b0022' } },
-  --     { Foreground = { Color = '#1b1032' } },
-  --     { Text = SOLID_RIGHT_ARROW },
-  --   },
-  -- },
+  use_fancy_tab_bar = false,
+  tab_max_width = 100,
+
+  window_padding = {
+    left = 5,
+    right = 5,
+    top = 2,
+    bottom = 2,
+  },
+
+  visual_bell = {
+    fade_in_function = 'Constant',
+    fade_in_duration_ms = 10,
+    fade_out_function = 'EaseOut',
+    fade_out_duration_ms = 75,
+  },
 }
